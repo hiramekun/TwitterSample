2d871c22d0c1fa6b87fd8a44dd2c2062baff2c39
hiramekun <thescript1210@gmail.com>
2017-05-13 19:59:59 +0900

    Add Views in PostTweetViewController

--- PostTweetViewController.swift
+++ PostTweetViewController.swift
@@ -4,6 +4,60 @@
 //
 
 import UIKit
+import SnapKit
 
 final class PostTweetViewController: UIViewController {
+    
+    // MARK: - Views -
+    
+    fileprivate lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .gray
+        return textField
+    }()
+    
+    
+    // MARK: - Initializer -
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+// MARK: - Life Cycle Events -
+
+extension PostTweetViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupLayout()
+    }
+}
+
+
+// MARK: - Setup -
+
+extension PostTweetViewController {
+    
+    fileprivate func setupView() {
+        view.addSubview(textField)
+    }
+    
+    fileprivate func setupLayout() {
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(view).inset(96)
+            make.left.equalTo(view).inset(32)
+            make.width.equalTo(200)
+            make.height.equalTo(32)
+        }
+    }
 }
