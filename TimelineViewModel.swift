86f70363e2c8b13eed47359a1a4e45ff0f6741aa
hiramekun <thescript1210@gmail.com>
2017-05-14 17:21:53 +0900

    Implement TimelineViewModel

--- TimelineViewModel.swift
+++ TimelineViewModel.swift
@@ -7,5 +7,55 @@ import Foundation
 import RealmSwift
 import RxSwift
 
-final class TimelineViewModel {
+protocol TimelineViewModelInputs {
+    var tweetContent: Variable<String> { get }
+}
+
+protocol TimelineViewModelOutputs {
+    var tweets: Variable<Results<Tweet>> { get }
+}
+
+protocol TimelineViewModelType {
+    var inputs: TimelineViewModelInputs { get }
+    var outputs: TimelineViewModelOutputs { get }
+}
+
+final class TimelineViewModel: TimelineViewModelInputs, TimelineViewModelOutputs {
+    
+    // MARK: - Properties -
+    
+    var inputs: TimelineViewModelInputs { return self }
+    var outputs: TimelineViewModelOutputs { return self }
+    let disposeBag = DisposeBag()
+    
+    
+    // MARK: - Inputs -
+    
+    let tweetContent = Variable<String>("")
+    
+    
+    // MARK: - Outputs -
+    
+    lazy var tweets: Variable<Results<Tweet>> = {
+        return Variable(try! Realm().objects(Tweet.self))
+    }()
+    
+    
+    // MARK: - Initializers -
+    
+    init() {
+        setupBindings()
+    }
+}
+
+
+extension TimelineViewModel {
+    
+    fileprivate func setupBindings() {
+        tweetContent.asObservable()
+            .subscribe(onNext: { string in
+                // TODO: save to realm in repository class
+            })
+            .disposed(by: disposeBag)
+    }
 }
