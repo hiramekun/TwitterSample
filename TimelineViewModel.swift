7b9b9a1e9ece1f41d742a063c4028358ab4762d9
hiramekun <thescript1210@gmail.com>
2017-05-14 13:47:24 +0900

    Add timeline ViewModel

---
+++ TimelineViewModel.swift
@@ -0,0 +1,11 @@
+//
+// Created by Takaaki Hirano on 2017/05/14.
+// Copyright (c) 2017 hiramekun. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import RxSwift
+
+final class TimelineViewModel {
+}
