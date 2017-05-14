5c3271e4c57a5c5725919a5c5c3e09d16494558f
hiramekun <thescript1210@gmail.com>
2017-05-14 10:38:52 +0900

    Reflect review in AppDelegate

--- TwitterSample/AppDelegate.swift
+++ TwitterSample/AppDelegate.swift
@@ -21,7 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UINavigationController(
-            rootViewController: TimelineViewController())
+            rootViewController: TimelineViewController()
+        )
         window?.makeKeyAndVisible()
         
         return true
