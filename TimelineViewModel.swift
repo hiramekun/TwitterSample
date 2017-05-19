fc970c706dac4000a18ba1fb02507aff7487a033
hiramekun <thescript1210@gmail.com>
2017-05-19 14:32:47 +0900

    Implement method to notify realm change

--- TimelineViewModel.swift
+++ TimelineViewModel.swift
@@ -7,8 +7,15 @@ import Foundation
 import RealmSwift
 import RxSwift
 
+enum RealmChange<T:RealmSwift.Object> {
+    case initial(results: Results<T>)
+    case deletions(rows: [Int])
+    case insertions(rows: [Int])
+    case modifications(rows: [Int])
+}
+
 protocol TimelineViewModelOutputs {
-    var tweets: Variable<Results<Tweet>> { get }
+    var tweets: BehaviorSubject<RealmChange<Tweet>> { get }
 }
 
 protocol TimelineViewModelType {
@@ -20,11 +27,40 @@ final class TimelineViewModel: TimelineViewModelOutputs {
     // MARK: - Properties -
     
     var outputs: TimelineViewModelOutputs { return self }
+    private var token: NotificationToken?
+    private let results = try! Realm().objects(Tweet.self)
     
     
     // MARK: - Outputs -
     
-    lazy var tweets: Variable<Results<Tweet>> = {
-        return Variable(try! Realm().objects(Tweet.self))
+    internal lazy var tweets: BehaviorSubject<RealmChange<Tweet>> = {
+        return BehaviorSubject<RealmChange<Tweet>>(
+            value: .initial(results: self.results)
+        )
     }()
+    
+    // MARK: - Life Cycle Events -
+    
+    init() {
+        token = results.addNotificationBlock { [weak self] change in
+            guard let tweets = self?.tweets, let results = self?.results else { return }
+            
+            switch change {
+            case .initial(results):
+                tweets.onNext(.initial(results: results))
+            case .update(results, let deletions, let insertions, let modifications):
+                tweets.onNext(.deletions(rows: deletions))
+                tweets.onNext(.insertions(rows: insertions))
+                tweets.onNext(.modifications(rows: modifications))
+            case .error(let error):
+                tweets.onError(error)
+            default:
+                break
+            }
+        }
+    }
+    
+    deinit {
+        token?.stop()
+    }
 }
